ad53dda3105bc8214f7e3c5e2e2737ac943460aa
Oğuzhan Ağır <90545225+oguzhanagir@users.noreply.github.com>
2024-11-07 00:21:03 +0300

    Added carter, mediatr building blocks

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Description = request.Description,
+            Category = request.Category,
+            ImageFile = request.ImageFile,
+            Price = request.Price
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
